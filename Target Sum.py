ac9e4dc02dd91b6d7d110bf3334a71f01e7839eb
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-17 07:22:44 +0800

    Add files via upload

---
+++ Target Sum.py
@@ -0,0 +1,17 @@
+#Target Sum
+
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        _len = len(nums)
+        dp = [collections.defaultdict(int) for _ in range(_len + 1)]
+        dp[0][0] = 1
+        for i, num in enumerate(nums):
+            for sum, cnt in dp[i].items():
+                dp[i + 1][sum + num] += cnt
+                dp[i + 1][sum - num] += cnt
+        return dp[_len][S]
